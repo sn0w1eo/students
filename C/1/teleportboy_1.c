30c534b57ba781ebc48f85efad842cf0ef32d1d9
teleport <boyteleport@gmail.com>
2020-03-10 13:10:46 +0600

    Error

--- C/1/teleportboy_1.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-#include "stdlib.h"
-int main()
-{
-	system("chcp 1251 >nul");
-	printf_s("\n\n\n********************************Кирилл********************************\n\n\n");
-	system("dir c:");
-	return 0;
-}
