ea423ced68c1cb69b7e128759a2f38d6e4835af5
Nurik <nurdoolot.tuuganbai@gmail.com>
2020-03-10 20:53:22 +0600

    not checked yet

---
+++ Loading.c
@@ -0,0 +1,11 @@
+﻿#include "stdio.h"
+#include "stdlib.h"
+#define name "Nurik"
+#define disk "dir C:\\"
+
+int main()
+{
+	printf("My name is %s.\n ", name);
+	system("Drive files %d \n", disk);
+	return 0;
+}
