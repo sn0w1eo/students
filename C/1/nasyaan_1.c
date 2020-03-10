a7ba388e958ead724809a885a4ba66679a0febcb
nasyaan <nasyaan@gmail.com>
2020-03-10 21:55:32 +0600

    first modul

---
+++ C/1/nasyaan_1.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "stdlib.h"
+int main()
+{
+	printf("\n Nastya \n");
+	printf("\n The program displays a list of C drive files. \n \n ");
+	system("dir C:\\");
+
+
+}
+	
