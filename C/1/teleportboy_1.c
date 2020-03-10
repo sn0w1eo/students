6b576ddfeaf9e7fff5c1409e3689cef5018b4266
teleport <boyteleport@gmail.com>
2020-03-10 18:51:29 +0600

    fixed

--- C/1/teleportboy_1.c
+++ C/1/teleportboy_1.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 int main()
-{	
+{		
+	puts("\n\n******************The program displays list of folders in disk C****************");
 	printf_s("\n\n*************************************%s*************************************\n\n", name);	
 	system("dir C:\\");
 	return 0;
