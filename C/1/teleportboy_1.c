c96c9a6af2d372cc030501f7f1bbbb573e3664f8
teleport <boyteleport@gmail.com>
2020-03-10 12:59:55 +0600

    completed task of the first lesson

---
+++ C/1/teleportboy_1.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+int main()
+{
+	system("chcp 1251 >nul");
+	printf_s("\n\n\n********************************Кирилл********************************\n\n\n");
+	system("dir c:");
+	return 0;
+}
