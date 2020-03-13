683142da16003e21f9e9588070fbbfc86bc82b3d
nasyaan <nasyaan@gmail.com>
2020-03-13 21:58:59 +0600

    2

---
+++ C/2/nasyaan_2.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+int main()
+{
+
+	char key;
+	int digit1, digit2, add, sub, mul, mod;
+	printf("\n1) Press any key... ");
+	key = getch();
+	drawTableKod(key);
+
+	printf("\n2) Enter 2 digits...");
+	scanf_s("%d %d", &digit1, &digit2);
+	add = digit1 + digit2;
+	sub = digit1 - digit2;
+	mul = digit1 * digit2;
+	double div = (double)digit1 / (double)digit2;
+	mod = digit1 % digit2;
+	drawTableMath(add, sub, mul, div, mod);
+	printf("\n100%%Complete...\n");
+}
+int drawTableKod(char key)
+{
+printf("\nÚÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄ¿");
+printf("\n³%10s³%10s³%10s³%10s³","ASCII","DEC", "HEX", "BINARY");
+printf("\nÃÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄ´ ");
+printf("\n³%10c³%10d³%10x³%10s³", key, key, key, "EMPTY");
+printf("\nÀÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÙ");
+
+
+}
+int drawTableMath(int add, int sub, int mul, double div, int mod)
+{
+	printf("\nÚÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄÂÄÄÄÄÄÄÄÄÄÄ¿");
+	printf("\n³%10s³%10s³%10s³%10s³%10s³", "ADD", "SUB", "MUL", "DIV", "MOD");
+	printf("\nÃÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄÅÄÄÄÄÄÄÄÄÄÄ´ ");
+	printf("\n³%10d³%10d³%10d³%10.1f³%10d³",add , sub, mul, div, mod);
+	printf("\nÀÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÙ");
+}
