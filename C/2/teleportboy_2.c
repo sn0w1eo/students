a923f72d2020267f3fbeca570f5e5d2ef57359ff
teleport <boyteleport@gmail.com>
2020-03-14 13:29:27 +0600

    fixed

--- C/2/teleportboy_2.c
+++ C/2/teleportboy_2.c
@@ -25,6 +25,7 @@ void SymbolTable(char symbol)
         "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA"
         "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA"
         "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC");//BC => ' ╝ ';
+    return;
 }
 void MathTable(int frstNumber, int scndNumber)
 {
@@ -35,51 +36,29 @@ void MathTable(int frstNumber, int scndNumber)
     double div = (double)frstNumber / scndNumber;
     int mod = frstNumber % scndNumber;
 
-    //╔═════════╦══════════╦══════════╦══════════╦══════════╗
-    printf_s("\n\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCB" //C9 => ' ╔ '; CD =>  ' ═ '; CB => ' ╦ '
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCB"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCB"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCB"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB");//BB => '╗';
-
-    //║                      ║                        ║                        ║                        ║                        ║
-    printf_s("\n\xBA      ADD\xBA       SUB\xBA       MUL\xBA       DIV\xBA       MOD\xBA");  //BA => ' ║ ';
-
-    //╠═════════╬══════════╬══════════╬══════════╬══════════╣
-    printf_s("\n\xCC\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCE" // CC => ' ╠ '; CE => ' ╬ ';
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCE"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCE"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCE"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xB9");//B9 => ' ╣ ' ; 
-
-    //║                      ║                        ║                        ║                        ║                        ║
-    printf_s("\n\xBA %8i\xBA  %8i\xBA%10i\xBA  %8.2lf\xBA  %8i\xBA", add, sub, mul, div, mod); //BA => ' ║ ';
-
-    //╚═════════╩══════════╩══════════╩══════════╩══════════╝
-    printf_s("\n\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA" //C8 =>' ╚ '; CA =>' ╩ ';
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCA"
-        "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC"); //BC => ' ╝ ';
+    
+    printf_s("\n╔═════════╦══════════╦══════════╦══════════╦══════════╗");    
+    printf_s("\n║      ADD║       SUB║       MUL║       DIV║       MOD║");      
+    printf_s("\n╠═════════╬══════════╬══════════╬══════════╬══════════╣");    
+    printf_s("\n║ %8i║  %8i║%10i║  %8.2lf║  %8i║", add, sub, mul, div, mod);     
+    printf_s("\n╚═════════╩══════════╩══════════╩══════════╩══════════╝"); 
+    return;
 }
-
-
-int main()
+int main(void)
 {  
     system("chcp 866");
     char symbol; int frstNumber, scndNumber;
+
     printf_s("\nHello!\nPlease, enter the SYMBOL:  ");    
     symbol = getch();
     //Calling the SymbolTable function
-    SymbolTable(symbol);
-
-    
+    SymbolTable(symbol);  
 
     printf_s("\nPlease, enter TWO NUMBERS:  ");
     scanf_s("%i%i", &frstNumber, &scndNumber);
-
     //Calling the Math Table Function
     MathTable(frstNumber, scndNumber);
 
     printf_s("\n100%% Complete...\n\n\n");
+    return 0;
 }
