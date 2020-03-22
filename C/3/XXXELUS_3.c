6e7f2c4492a81c83b9f343e9b783f0bbc9dce4dc
Konstantin <xxxelus@gmail.com>
2020-03-22 14:33:55 +0600

    Fixed.Kirill helped

--- C/3/XXXELUS_3.c
+++ C/3/XXXELUS_3.c
@@ -1,265 +1,94 @@
 #include "stdio.h"
-#include "stdlib.h"
-#include <string.h>
 
 
-//КТО СПИЗДИТ ТОТ ЛОХ (who spizdit that sucker)
+int main()
+{
+	int firstDig, secondDig, mathSign, firstNumber = 0, secondNumber = 0, Result;
+	printf_s("Universal calculator...\n"
+		"Syntax: VALUE SIGN VALUE EQUAL\n"
+		"----------------------------------------\n"
+		"   VALUE: any numeric value\n"
+		"    SIGN: +, -, / , *, %%, ^, | , &\n"
+		"   EQUAL: =\n"
+		"----------------------------------------\n"
+		"     EXP: ");
+	do
+	{
+		int count = 0;
+		firstDig = getch();
+		if (firstDig >= '0' && firstDig <= '9')
+		{
+			putchar(firstDig);
+			firstNumber = firstNumber * 10 + (firstDig - 48);
+		}
+		if (firstNumber > 0 && (firstDig == '-' || firstDig == '+'
+			|| firstDig == '/' || firstDig == '%' || firstDig == '*'
+			|| firstDig == '^' || firstDig == '|' || firstDig == '&'))
+		{
+			putchar(firstDig);
 
-//немного пояснил за код,в остальном все CTRL+C CTRL+V 
-//P.S Сделал через If ,а не через switch , потому что Кирюха начал делать свитчом,что бы хоть шушут было отличие
+			
+			mathSign = firstDig;
+			break;
+		}
+	} while (1);
+
+	do
+	{
+		secondDig = getch();
+		if (secondDig >= '0' && secondDig <= '9')
+		{
+			putchar(secondDig);
+			secondNumber = secondNumber * 10 + (secondDig - 48);
+		}
+		if (secondNumber > 0 && secondDig == '=')
+		{
+			putchar(secondDig);
+			break;
+		}
+	} while (1);
+	Result = mathOperation(mathSign, firstNumber, secondNumber);
+	
+	if (mathSign == '/')
+	{
+		printf_s("%.2lf", (double)firstNumber / secondNumber);
+		printf_s("\n----------------------------------------");
+		printf_s("\nError");
+	}
+	else
+	{
+		printf_s("%d", Result);
+		printf_s("\n----------------------------------------"
+
+			"\nHEX: %.8X", Result);
+		printf_s("\nBIN: ");
+		decToBinary(Result);
+	}
+	return 0;
+}
 int decToBinary(int n)
 {
-	
+
 	for (int i = 31; i >= 0; i--) {
 		int k = n >> i;
 		if (i == 7)printf(" ");
 		if (i == 15)printf(" ");
 		if (i == 23)printf(" ");
 		if (k & 1)
-			printf( "1");
+			printf("1");
 		else
 			printf("0");
 	}
 }
-
-int main()
+int mathOperation(int mathSign, int firstNumber, int secondNumber)
 {
-	system("chcp 866>nul");
-	decor();
-	
-	char inputSymbol;
-	char firstNumberArray[10];
-	char secondNumberArray[10];
-	int secondNumber = 0, firstNumber = 0, Counter = 0;
-	int result;
-	do//пока 
-	{
-		inputSymbol = getch();
-		if (inputSymbol >= '0' && inputSymbol <= '9' || inputSymbol == '+' || inputSymbol == '-' 
-			|| inputSymbol == '*' || inputSymbol == '|' || inputSymbol == '/' || inputSymbol == '%' 
-			|| inputSymbol == '&' || inputSymbol == '^')// Если вводится один из символов
-		{
-		
-			putchar(inputSymbol);//печатаем
-			if (inputSymbol >= '0' && inputSymbol <= '9')
-			{
-				firstNumberArray[Counter] = inputSymbol;//массив куда идут цифры первого числа
-				firstNumber = atoi(firstNumberArray);//Конвертим цифры в число
-				
-			}
-			Counter++;
-		}
-	} while ((inputSymbol) != '+' && (inputSymbol) != '-' && (inputSymbol) != '*' && (inputSymbol) != '/' && (inputSymbol) != '%' && (inputSymbol) != '&' && (inputSymbol) != '|' && (inputSymbol) != '^');
-
-	Counter = 0;
-	
-		if (inputSymbol == '+') {//ecли встречаем знак ,то начинаем записывать 2е 
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;//массив второго числа
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');//выполняем действие что сверху пока не наткнемся на =
-			if (inputSymbol == '=')//если жмякнули = то выводим
-			{
-				result = (double)firstNumber + secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-				
-			}
-		}
-		//Остальное все аналогично
-		if (inputSymbol == '-') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = (double)firstNumber - secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '*') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = (double)firstNumber * secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '/') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				double result = (double)firstNumber / (double)secondNumber;
-				printf_s("=%.2f\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '%') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = firstNumber % secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '^') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = firstNumber ^ secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '|') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = firstNumber | secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-		if (inputSymbol == '&') {
-			do
-			{
-				inputSymbol = getch();
-				if (inputSymbol >= '0' && inputSymbol <= '9')
-				{
-					putchar(inputSymbol);
-					if (inputSymbol >= '0' && inputSymbol <= '9')
-					{
-						secondNumberArray[Counter] = inputSymbol;
-						secondNumber = atoi(secondNumberArray);
-					}
-					Counter++;
-				}
-			} while ((inputSymbol) != '=');
-			if (inputSymbol == '=')
-			{
-
-				result = firstNumber & secondNumber;
-				printf_s("=%d\n"
-					"HEX: %x\n"
-					"BINARY:", result, result); decToBinary(result);
-			}
-		}
-	
+	if (mathSign == '+') return firstNumber + secondNumber;
+	else if (mathSign == '-') return firstNumber - secondNumber;
+	else if (mathSign == '*') return firstNumber * secondNumber;
+	else if (mathSign == '/') return (double)firstNumber / secondNumber;
+	else if (mathSign == '%') return firstNumber % secondNumber;
+	else if (mathSign == '^') return firstNumber ^ secondNumber;
+	else if (mathSign == '|') return firstNumber | secondNumber;
+	else return firstNumber & secondNumber;
+	return 0;
 }
-
-	int decor()
-	{
-		printf("░░░░░░░░░░▒▒▒▒▒▒▒▓▓▓▓ XXXELUS_CALC ▓▓▓▓▒▒▒▒▒▒▒░░░░░░░░░░\n"
-		    "\nУниверсальный калькулятор...\n"
-			"Синтаксис: ЗНАЧЕНИЕ ЗНАК ЗНАЧЕНИЕ РАВНО\n"
-			"----------------------------------------\n"
-			"Допустимые символы:\n"
-			"     ЗНАЧЕНИЕ: любые числа\n"
-			"     ЗНАК: + , - , / , * , % , ^ , | , & \n"
-			"     РАВНО : =\n"
-			"----------------------------------------\n"
-			"     РЕШЕНИЕ: ");
-	}
-	
