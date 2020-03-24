27fd888ced985ea00927d7e6c068309a4c7b938c
Konstantin <xxxelus@gmail.com>
2020-03-24 19:42:35 +0600

    #1 Modul_4

--- C/4,5/XXXELUS_4.c
+++ C/4,5/XXXELUS_4.c
@@ -1,24 +1,79 @@
-#include <iostream>
-#include <conio.h>
-#include <windows.h>
+#include "windows.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-int main()
+
+#define setBlue case 0x31:SET_COLOR(0x1) break;
+#define setGreen case 0x32: SET_COLOR(0x2) break;
+#define setGoluboi case 0x33:SET_COLOR(0x3) break;
+#define setRed case 0x34: SET_COLOR(0x4) break;
+#define setLilac case 0x35:SET_COLOR(0x5) break;
+#define setYellow case 0x36:SET_COLOR(0x6) break;
+#define setWhite case 0x37:SET_COLOR(0x7) break;
+#define setLightBlue case 0x38:SET_COLOR(0x9) break;
+#define setLightLilac case 0x39:SET_COLOR(0xd) break;
+#define RIGHT 0x4D00
+#define LEFT 0x4B00
+#define UP 0x4800
+#define DOWN 0x5000
+#define ESC 0x1B
+#define GO(x,y){COORD coordinates={x,y}; SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coordinates);}
+#define SET_COLOR(color){SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);}
+
+
+
+int main() 
 {
-    int x = 0, y = 0;
-    int step = 10;
-    char c;
+	system("chcp 866>nul");
+	int x = 0, y = 0, xMax = 121,yMax=30;
+	int push;
+	do {
 
-    SetCursorPos(x, y);
-    do {
-        c = getch();
-        switch (c) {
-        case 75: SetCursorPos(x -= step, y); break;
-        case 77: SetCursorPos(x += step, y); break;
-        case 72: SetCursorPos(x, y -= step); break;
-        case 80: SetCursorPos(x, y += step); break;
-        }
-    } while (c != 27); // Esc
-
-    system("pause");
-    return 0;
+		push = getch();
+		if (push == 0 || push == 0xE0) 
+		{
+			push = getch()<<8;
+		}
+		if (push >= 'A' && push <= 'z') { printf("%c\b", push); }
+		switch (push)
+		{
+		case RIGHT: if(x!=xMax) x++; GO(x, y) break;
+		case LEFT: if(x!=0) x--; GO(x, y) break;
+		case UP: if(y!=0) y--; GO(x, y) break;
+		case DOWN: if(y!=yMax) y++; GO(x, y) break;
+		setRed
+		setGreen
+		setBlue
+		setLightBlue
+		setYellow
+		setWhite
+		setLilac
+		setLightLilac
+		setGoluboi
+		}
+		
+		if (push == ESC) 
+		{
+			GO(0,30)
+				SET_COLOR(0x8)
+			printf("лллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллллл");
+				
+				for (int i = 0; i <= 110;i++) 
+				{
+					int dec = 58;
+					while (dec <= 122) 
+					{
+						SET_COLOR(0xa)
+						GO(i, 30);
+						printf("%c\b", dec);
+						dec++;
+						printf("\xDB\b");
+						
+					}
+					if (i == 110) { printf("\nComplete 100%%....\n\n"); }
+				}
+				
+		}
+	} while (push != ESC);
+	return 0;
 }
