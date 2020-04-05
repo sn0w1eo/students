c16ec00ba20714c4e1b3d976e7ff998468a0fcd6
karlos312 <bkominarets@gmail.com>
2020-04-05 21:18:19 +0600

    #2 fixed

--- C/6/Bogdan_1.c
+++ C/6/Bogdan_1.c
@@ -1,310 +1,130 @@
-#include "windows.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
-
-#define xMax  39
-#define yMax  19
-#define ESC   0x1B
-#define LEFT  0xE04B
-#define RIGHT 0xE04D
-#define UP    0xE048
-#define DOWN  0xE050
-
-
-//смена позиции
-void position(int x, int y) {
-	COORD coordinat = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinat);
-}
-
-
-//смена цвета
-void color(int ColorNumber) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ColorNumber);
-}
-
-//вывод рамки
-void Display(int x, int y)
-
-{//очистка экрана
-	system("cls");
-	position(x, y);
-	color(13);
-
-	//верхняя граница
-	for (int i = 0; i < xMax; i++)
-	{
-		position(x, y);
-		printf("Д");
-		x++;
-	}
-
-	//правая граница
-	for (int i = 0; i < yMax; i++)
-	{
-		position(x, y);
-		printf("і");
-		y++;
-	}
-
-	//Нижняя граница
-	for (int i = xMax; i > 0; i--)
-	{
-		position(x, y);
-		printf("Д");
-		x--;
-	}
-
-	//Углы
-	position(0, 0);
-	printf("Ъ");
-	position(xMax, 0);
-	printf("ї");
-	position(xMax, yMax);
-	printf("Щ");
-	position(0, yMax);
-	printf("А");
-}
-
-//вывод массива с соответствующими цветами
-void PrintMassiv(int Massiv[][xMax-1]){
-
-	position(0, 1);
-	for (int y = 0; y < yMax-1 ; y++) {
-		for (int x = 0; x < xMax-1 ; x++) {
-			if (x == 0)
-			{
-				color(13);
-			}
-			else if (Massiv[y][x] == '#')
-			{
-				color(12);
-			}
-			else if (Massiv[y][x] == '*')
-			{
-				color(11);
-			}
-			else if (Massiv[y][x] == '@')
-			{
-				color(10);
-			}
-			else if (Massiv[y][x]==0xF9)
-			{
-				color(14);
-			}
-			printf("%c", Massiv[y][x]);
-		}
-		printf(" \n");
-	}
-	}
+/*itoa
+ltoa
+gcvt
+atoi
+atol
+strtol
+strtod
 
 
 
-int main() {
+string.h
+strlen
+strcmp
+strncmp
+strcpy
+strncpy
+strcat
+strncat*/
 
-	char FirstName[20] = { 0x00 };
-	char LastName[20] = { 0x00 };
-	char NickName[20] = { 0x00 };
-	int Steps, Blocks, x = 0, y = 0;
-	int Massiv[yMax-1][xMax-1];
-	unsigned int vvod;
-	int stroka = 0, stolb = 1;
-	srand(time(NULL));
-	int lable=1;
+#define _CRT_SECURE_NO_WARNINGS
+#include  "stdio.h"
+#include  "stdlib.h"
+#include  "string.h"
+
+#define size 100
+
+//вот мой говнокод)))
+int main()
+{
 	
+	int chislo;
+	double chislo2;
+	char numbers[size] ; 
+	char str1[size];
+	char str2[size] = {0x00};
 
 
-	//Ввод данных
-	printf("first Name: ");
-	gets(FirstName);
-	printf("\nlastName: ");
-	gets(LastName);
-	printf("\nnick Name: ");
-	gets(NickName);
-	printf("\nWelcome %s \"%s\" %s\n", FirstName, NickName, LastName);
-	printf("Blocks: ");
-	scanf_s("%d", &Blocks);
-	printf("Steps: ");
-	scanf_s("%d", &Steps);
-	int BeginnerSteps = Steps;
-	Display(x, y);
-	position(x, yMax + 2);
-	color(14);
-	printf("STEPS: %d", Steps);
+
+
+
+	//strtol
+	printf("vvedite chislo\n");                           
+	gets(numbers);
+	long longNumber = strtol(numbers, NULL, 10);                //функцию не cовсем понял но она преобразует строку в число типа long преобразуя в нужную систему счисления
+	printf("%lf", longNumber);
 	
+	//strtod
+	printf("\n\nvvedite chislo\n");
+	gets(numbers);
+	double doubleNumber = strtod(numbers, NULL, 10);           //функцию не cовсем понял но она преобразует строку в число типа double преобразуя в нужную систему счисления
+	printf("%ld", doubleNumber);
 
-	//Заполнение Массива пробелами
-	for (int y = 0; y < yMax-1 ; y++) {
-		for (int x = 0; x < xMax-1; x++) {
-			Massiv[y][x] = ' ';
-		}
-		if (x == 0)
-		{
-			Massiv[y][x] = 'і';
-		}
-	}
+	//atoi
+	printf("\n\nvvedite chislo,poluchite kvadrat\n");
+	gets(numbers);
+	printf("%d\n", atoi(numbers)* atoi(numbers));              //преобразует строку в число типа int
 
-
-	//Заполнение массива @
-	for (int i = 1; i > 0; i--)
-	{
-
-		y = rand() % (yMax -1);
-		x = rand() % (xMax -1);
-		if (x == 0 || (x == 1 && y == 0))
-		{
-			++i;
-			continue;
-		}
-		Massiv[y][x] = '@';
-	}
+	//atol
+	printf("\nvvedite chislo,poluchite summ\n");
+	gets(numbers);
+	printf("%d\n", atol(numbers) + atol(numbers));             //преобразует строку в число типа long
 
 
-	//Заполнение массива *
-	Massiv[0][1] = '*';
+	//_itoa_s
+	printf("\nvvedite chislo,poluchite hex\n");
+	scanf_s("%d", &chislo);
+	char per[size];
+	_itoa_s(chislo, numbers, size,16);                        //преобразует число в строку и после в нужную систему счисления от 2 до 36 . 1)Число, которое требуется преобразовать. 2)Выходной буфер, содержащий результат преобразования. 3)Размер буфера в символах или расширенных символах.4)Основание системы счисления
+	printf(numbers);
+
+	//_ltoa_s
+	printf("\n\nvvedite chislo,poluchite bin\n");
+	scanf_s("%d", &chislo);
+	_ltoa_s(chislo, numbers, size,2);                        //преобразует число(длинное) в строку и после в нужную систему счисления от 2 до 36
+	printf(numbers);
 
 
-	//заполнение массива (#)
-	for (int i = Blocks; i > 0; i--)
-	{
-		y = rand() % (yMax-1 );
-		x = rand() % (xMax -1);
-		if (x == 0 || (x == 1 && y == 0) || Massiv[y][x] == '@'|| Massiv [y] [x]=='#') {
-			++i;
-			continue;
-		}
-		Massiv[y][x] = '#';
-	}
+	//_gcvt_s
+	printf("\n\nvvedite drobnoye chislo\n");
+	scanf_s("%lf", &chislo2); 
+	_gcvt_s(numbers, size, chislo2,2);                      // Преобразует значение с плавающей запятой в строку.  1)Буфер для сохранения результата преобразования. 2)Размер буфера.3)Преобразуемое значение.4)Количество значащих цифр хранятся.
+	printf(numbers);
 
-	//вывод изначальног массива
-	PrintMassiv(Massiv);
-	
-	
-	
-	do
-	{
+	//strlen
+	printf("\n\nDlina stroki  % s - % d simvolov\n", numbers, strlen(numbers));      //измерение длинны строки
 
-		vvod = _getch();
 
-		if (vvod == 0xE0)
-		{
-			vvod = vvod << 8;
-			vvod = vvod | _getch();
-		}
-		//проверка стрелок
-		switch (vvod)
-		{
-		case RIGHT: 
-			//границы и блоки
-			if ((Massiv[stroka][stolb + 1] == '#')||(stolb+1==xMax-1))
-		{
-			break;
-		}
-				else
-		{
-			Massiv [stroka][stolb] = 0xF9;                 //знак точки
-			if (Massiv[stroka][stolb + 1] == '@') {        //проверка на финиш
-				lable = 0;
-			}
-			++stolb;
-			Massiv[stroka][stolb] = '*';
-			Display(0, 0);                                 //вывод рамки
-			PrintMassiv(Massiv);                           //вывод массива
-			Steps--;
-			position(0, yMax + 2);                         //вывод кол-ва шагов
-			color(14);
-			printf("STEPS: %d", Steps);
-			break;
-
-		}
-		case LEFT: if (Massiv[stroka][stolb - 1] == '#' || (stolb - 1 == 0))
-		{
-			break;
-		}
-				   else
-		{
-			Massiv[stroka][stolb] = 0xF9;
-			if (Massiv[stroka][stolb - 1] == '@')
-				lable = 0;
-			--stolb;
-			Massiv[stroka][stolb] = '*';
-			Display(0, 0);
-			PrintMassiv(Massiv);
-			Steps--;
-			position(0, yMax + 2);
-			color(14);
-			printf("STEPS: %d", Steps);
-			break;
-		}
-
-		case UP: if (Massiv[stroka - 1][stolb] == '#' || (stroka - 1 == -1))
-
-		{
-			break;
-		}
-				 else
-		{
-			Massiv[stroka][stolb] = 0xF9;
-			if (Massiv[stroka-1][stolb ] == '@')
-				lable = 0;
-			--stroka;
-			Massiv[stroka][stolb] = '*';
-			Display(0, 0);
-			PrintMassiv(Massiv);
-			Steps--;
-			position(0, yMax + 2);
-			color(14);
-			printf("STEPS: %d", Steps);
-			break;
-		}
-
-		case DOWN: if (Massiv[stroka + 1][stolb] == '#' || (stroka + 1 == yMax - 1))
-
-		{
-			break;
-		}
-				   else
-		{
-			Massiv[stroka][stolb] = 0xF9;
-			if (Massiv[stroka-1][stolb ] == '@')
-				lable = 0;
-			++stroka;
-			Massiv[stroka][stolb] = '*';
-			Display(0, 0);
-			PrintMassiv(Massiv);
-			Steps--;
-			position(0, yMax + 2);
-			color(14);
-			printf("STEPS: %d", Steps);
-			break;
-		}
-		}
-		if (vvod == ESC) {
-			position(0, yMax+2);
-			color(13);
-			printf("Ok buddy. Cya next time...");
-			_getch();
-			return;
-		}
-
-	} while (Steps !=0 && lable!=0);
-
-	
-	if (lable == 0)
-	{
-		position(0, yMax+2);                                                        
-		color(10);
-		printf("Congratulations! You've won! Steps: %d/%d ",Steps,BeginnerSteps);
-	}
+	//strcmp
+	printf("\nvvedite stroky1: ");
+	gets(str1);
+	gets(str1);
+	printf("vvedite stroky2: ");
+	gets(str2);
+	if (strcmp(str1, str2) == 0)                           //сравнивает 2 строки по алфавиту если строки одинаковы выдает 0
+		puts("troki odinakovy\n");
 	else
-	{
-
-		position(0, yMax + 2);
-		color(12);
-		printf("You've failed. Try ur luck next time");
-	}
+		puts("stroki otlichayutsya\n");
 
 
-	_getch();
+	//strncmp
+	if (strncmp(str1, str2,1) == 0)                        //сравнивает 2 строки по алфавиту,не более определенного кол-ва символов. если строки одинаковы выдает 0
+		puts("stroki odinakovy\n");
+	else
+		puts("stroki otlichayutsya\n");
+
+	//strcpy
+	strcpy(str1, "kopirovaniye vypolneno uspeshno\n");     //копирование строки целиком
+	printf(str1);
+
+	//strncpy
+	printf("\n vyvod strncpy\n");
+	strncpy(str2, str1,5);                                 //копирование строки до определенного символа
+	printf(str2);
+
+	//strcat
+	printf("\n\n vyvod strcat\n");                        //присоединение строки
+	strcat(str2, str1);
+	printf(str2);
+
+	//strncat
+	printf("\n vyvod strncat\n");                       //присоединение строки до опрределенного символа
+	strncat(str2, str1,10);
+	printf(str2);
+
+
+
+	return 0;
 }
+
+
