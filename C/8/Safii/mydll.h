9ee0d41b3f1cc06b87a8b2853359bbe611a12afe
Safi_Nova <novellacity12@gmail.com>
2020-04-19 19:41:29 +0600

    #6 done

--- C/8/Safii/mydll.h
+++ C/8/Safii/mydll.h
@@ -1,6 +1,6 @@
-#include "stdio.h"
-#ifndef MYDLL.H
-#define MYDLL.H
+
+#ifndef MYDLL_H
+#define MYDLL_H
 __declspec(dllexport) void f1();
 __declspec(dllexport) void f2();
 __declspec(dllexport) void f3();
