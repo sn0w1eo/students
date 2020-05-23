fa4148f1bc7115edb153d898d785d6c001a3cf64
karlos312 <bkominarets@gmail.com>
2020-05-23 21:47:46 +0600

    #9sort

---
+++ C/12/Bogdan_12/main.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "algorithm.h"
+
+
+
+
+int main() {
+	
+
+	data db = initStorage(20);
+	addPerson(&db, "Bogdan", 29, 123.123);
+	addPerson(&db, "Dima", 13, 136.123);
+	addPerson(&db, "Artur", 234, 1.123);
+	addPerson(&db, "Ira", 45, 123.233);
+	addPerson(&db, "Kirill", 5, 14.123);
+
+
+	sort(&db, byEarnings, ascending);         // byName, byAge, byEarnings //ascending ,descending 
+	
+	printStorage(&db);
+	freeStorage(&db);
+
+
+}
