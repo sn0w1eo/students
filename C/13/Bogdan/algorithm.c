f3c38449c208ab2decb52b1600c36978ce6116f1
karlos312 <bkominarets@gmail.com>
2020-06-07 13:38:33 +0600

    #10

---
+++ C/13/Bogdan/algorithm.c
@@ -0,0 +1,198 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+#include "algorithm.h"
+
+int count = 0;
+
+
+
+data initStorage(size_t size) {
+	data db;
+	db.size = size;
+	db.persons = calloc(size, sizeof(person));
+	if (db.persons == NULL) {
+		printf("fucked up");
+		return db;
+	}
+	return db;
+}
+
+
+void addPerson(data* db, char* name, int age, double earnings) {
+	for (size_t i = 0; i < db->size - 1; i++) {
+		if (db->persons[i].name != NULL) continue;
+		setName(&db->persons[i], name);
+		setAge(&db->persons[i], age);
+		setEarnings(&db->persons[i], earnings);
+		count++;
+		break;
+	}
+}
+
+void setEarnings(person* per, double earnings) {
+	per->earnings = earnings;
+}
+
+void setAge(person* per, int age) {
+	per->age = age;
+}
+
+void setName(person* per, char* name) {
+	per->name = _strdup(name);
+}
+
+int getName(person* per) {
+	return per->name;
+}
+
+int getAge(person* per) {
+	return per->age;
+}
+
+double getEarnings(person* per) {
+	return per->earnings;
+}
+
+void printStorage(data* db) {
+	for (size_t i = 0; i < db->size - 1; i++) {
+		if (db->persons[i].name != NULL) {
+			printf("name: %s\n", getName(&db->persons[i]));
+			printf("age: %d\n", getAge(&db->persons[i]));
+			printf("earnings: %.3f\n\n", getEarnings(&db->persons[i]));
+		}
+	}
+}
+
+void freeStorage(data* db) {
+	for (size_t i = 0; i < db->size - 1; i++) {
+		free(db->persons[i].name);
+		db->persons[i].age = 0;
+		db->persons[i].earnings = 0;
+	}
+	free(db->persons);
+	db->persons = NULL;
+	db->size = 0;
+
+}
+
+int compByNameDescending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return -strcmp(per->name, vtor->name);
+}
+int compByNameAscending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return strcmp(per->name, vtor->name);
+}
+
+int compByAgeDescending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return -((per->age) - (vtor->age));
+}
+
+int compByAgeAscending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return (per->age) - (vtor->age);
+}
+
+int compByEarningsDescending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return -((per->earnings) - (vtor->earnings));
+}
+
+int compByEarningsAscending(const void* a, const void* b) {
+	const person* per = a;
+	const person* vtor = b;
+	return (per->earnings) - (vtor->earnings);
+}
+
+void sort(data* db, int parametr, int sequence) {
+	switch (parametr)
+	{
+	case byName:
+		if (sequence == descending) {
+			qsort(db->persons, count, sizeof(person), compByNameDescending);
+		}
+		else {
+			qsort(db->persons, count, sizeof(person), compByNameAscending);
+		}
+		break;
+	case byAge:
+		if (sequence == descending) {
+			qsort(db->persons, count, sizeof(person), compByAgeDescending);
+		}
+		else {
+			qsort(db->persons, count, sizeof(person), compByAgeAscending);
+		}
+		break;
+	case byEarnings:
+		if (sequence == descending) {
+			qsort(db->persons, count, sizeof(person), compByEarningsDescending);
+		}
+		else {
+			qsort(db->persons, count, sizeof(person), compByEarningsAscending);
+		}
+		break;
+	}
+
+}
+
+int inFileTxt(data* db) {
+	FILE* stream = fopen("data1.txt", "w+");
+	
+	if (stream == NULL ) {
+		printf("go fuck");
+		return -1;
+	}
+	for (size_t i = 0; i < count; i++) {
+		fprintf(stream, "name: %s\n", getName(&db->persons[i]));
+		fprintf(stream, "age: %d\n", getAge(&db->persons[i]));
+		fprintf(stream, "earnings: %.3f\n\n", getEarnings(&db->persons[i]));
+	}
+	readFileTxt(stream);
+	
+	return 1;
+}
+
+int readFileTxt(FILE* stream) {
+	char buffer[1000];
+	rewind(stream);
+	while (fgets(buffer, 1000, stream)) {
+		printf("%s", buffer);
+	}
+	fclose(stream);
+}
+
+
+int inFileDat(data* db) {
+	FILE* stream = fopen("data2.dat", "wb");
+
+	if (stream == NULL) {
+		printf("go fuck");
+		return -1;
+	}
+	for (size_t i = 0; i < count; i++) {
+		fprintf(stream, "name: %s\n", getName(&db->persons[i]));
+		fprintf(stream, "age: %d\n", getAge(&db->persons[i]));
+		fprintf(stream, "earnings: %.3f\n\n", getEarnings(&db->persons[i]));
+	}
+	readFileTxt(stream);
+	return 1;
+}
+
+int readFileDat(FILE* stream) {
+	char buffer[1000];
+	rewind(stream);
+	while (fgets(buffer, 1000, stream)) {
+		printf("%s", buffer);
+	}
+	fclose(stream);
+}
+
---
+++ C/13/Bogdan/main.c
@@ -0,0 +1,35 @@
+
+#include "stdio.h"
+#include "algorithm.h"
+
+
+
+
+int main() {
+
+
+	data db = initStorage(20);
+	addPerson(&db, "Bogdan", 29, 123.123);
+	addPerson(&db, "Dima", 13, 136.123);
+	addPerson(&db, "Artur", 234, 1.123);
+	addPerson(&db, "Ira", 45, 123.233);
+	addPerson(&db, "Kirill", 5, 14.123);
+
+
+	sort(&db, byName, descending);         // byName, byAge, byEarnings //ascending ,descending 
+
+	//if (inFileTxt(&db) == -1) {
+	//	freeStorage(&db);
+	//	return -1;
+	//}
+	
+	if (inFileDat(&db) == -1) {
+		freeStorage(&db);
+		return -1;
+	}
+	freeStorage(&db);
+	//printStorage(&db);
+	
+
+
+}
