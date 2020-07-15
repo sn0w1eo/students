472ae2b44eb708b00e659a73440e7e36b4edd7c7
nasyaan <nasyaan@gmail.com>
2020-07-15 20:14:38 +0600

    files #10

--- C/13/nasyaan_13_2/nasyaan_13_re/main.c
+++ C/13/nasyaan_13_2/nasyaan_13_re/main.c
@@ -2,6 +2,7 @@
 
 #include "stdio.h"
 
+//структура контакт: номер контакта, имя, почта, номер телефона, индекс
 struct contactData {
 	int cntctNum;
 	char name[15];
@@ -9,7 +10,6 @@ struct contactData {
 	char phone[10];
 	char zipCode[7];
 };
-
 
 int main() {
 	FILE* cfPtr; //указатель файла
@@ -23,12 +23,12 @@ int main() {
 	}
 	else
 	{
-		printf("%s", "Enter contact number" " (1 to 100, 0 to end input)\n");
+		printf("%s", "Enter contact number." " (1 to 100, 0 to end input)\n");
 		scanf("%d", &contact.cntctNum);
 
 		while (contact.cntctNum != 0) {
 
-			printf("%s", "Enter contact name, email, phone number and zip code\n");
+			printf("%s", "Enter contact name, email, phone number and zip code.\n");
 			fscanf(stdin, "%s%s%s%s", contact.name, contact.email, contact.phone, contact.zipCode);
 			/*найти позицию указанного контакта*/
 			fseek(cfPtr, (contact.cntctNum - 1) *
@@ -40,9 +40,9 @@ int main() {
 		}
 		fclose(cfPtr);
 	}
-	errno_t err = fopen_s(&cfPtr, "phone.dat", "rb"); //открытие файла для записи в бинарном режиме
+	errno_t err = fopen_s(&cfPtr, "phone.dat", "rb"); //открытие файла для чтения в бинарном режиме
 		//проверка на ошибку
-	if (cfPtr == NULL || error) {
+	if (cfPtr == NULL || err) {
 		perror("Open file failed\n");
 		getch();
 		return -1;
@@ -61,7 +61,44 @@ int main() {
 		}
 		fclose(cfPtr);
 	}
-
-
+	errno_t errorTxt = fopen_s(&cfPtr, "phone.txt", "w"); //открытие файла для записи в текстовом режиме
+		//проверка на ошибку
+	if (cfPtr == NULL || errorTxt) {
+		perror("Open file failed\n");
+		getch();
+		return -1;
+	}
+	else
+	{
+		puts("Enter the contact number, name, email, phone number and zip code.");
+		puts("Enter EOF to end input.");
+		printf("%s", "? ");
+		scanf("%d%s%s%s%s", &contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+		if ( !feof (stdin) )
+		{
+			fprintf(cfPtr, "%d %s %s %s %s\n", contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+			printf("%s", "? ");
+			scanf("%d%s%s%s%s", &contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+		}
+		fclose(cfPtr);
+	}
+	errno_t errTxt = fopen_s(&cfPtr, "phone.txt", "r"); //открытие файла для чтения в текстовом режиме
+		//проверка на ошибку
+	if (cfPtr == NULL || errTxt) {
+		perror("Open file failed\n");
+		getch();
+		return -1;
+	}
+	else {
+		printf("%-10s%-16s%-21s%-11s%-8s\n", "Contact", "Name", "Email", "Phone", "ZIP code");
+		fscanf(cfPtr, "%d%s%s%s%s", &contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+		/*прочитать из файла все записи пока не eof */
+		while (!feof(cfPtr))
+		{
+			printf("%-10d%-16s%-21s%-11s%-8s\n", contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+			fscanf(cfPtr, "%d%s%s%s%s", &contact.cntctNum, contact.name, contact.email, contact.phone, contact.zipCode);
+		}
+		fclose(cfPtr);
+	}
 	return 0;
 }
